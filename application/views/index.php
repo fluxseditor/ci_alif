16a67d4bebd25f8b3026998cb40be621303ea477
fluxseditor <alifharyantosss@gmail.com>
2021-03-30 13:26:01 +0700

    initialize

---
+++ application/views/index.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<html>
+    <head>
+        <title>Latihan 2</title>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/bootstrap.css">
+        <script type="text/javascript" src="<?php echo base_url();?>assets/js/mylib.js"></script>
+    </head>
+    <body>
+        <h3 class="judul">Latihan 2</h3>
+        <p class="paragraf"> Menyisipkan file CSS, JS dan gambar dalam <strong>Codeigniter 3</strong></p>
+        <img src="<?php echo base_url();?>assets/images/tugu-jogja.jpg" onclick="klik()">
+    </body>
+</html>
---
+++ application/views/welcome_message.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="<?php echo base_url()?>/assets/css/bootstrap.css">
+<link rel="stylesheet" href="<?php echo base_url()?>/assets/css/custom.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+
+<body class="layar">
+
+<button class="open-button" onclick="openForm()">Open Form</button>
+
+<div class="form-popup" id="myForm">
+  <form action="/action_page.php" class="form-container">
+    <h1>Login</h1>
+
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit" class="btn">Login</button>
+    <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+  </form>
+</div>
+
+<script>
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+</script>
+
+</body>
+</html>
