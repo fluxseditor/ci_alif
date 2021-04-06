ed9b55912c78ca1fb51f847645807881026d674b
fluxseditor <alifharyantosss@gmail.com>
2021-04-07 01:55:06 +0700

    nambah crud 1 halaman

--- application/views/admin/data_ap.php
+++ application/views/admin/data_ap.php
@@ -82,6 +82,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <div class="jambut">
   <h1 style="padding-left: 170px">Data Siswa Administrasi Perkantoran (AP) SMK BPI</h1>
+  <button id="myBtn" class="btn-primary" style="float:right; right:30px; position: absolute">Tambah Siswa</button>
 <table style="width:100%" class="jambut">
       <tr>
         <th>No. </th>
@@ -105,7 +106,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
               <td><?php echo $row->alamat; ?></td>
               <td><?php echo $row->telp; ?></td>
               <td>
-              <?php echo anchor('Welcome/edit/'.$row->id,'Edit'); ?> |
+              <?php echo anchor('Welcome/edit/'.$row->id,'Edit'); ?> | 
               <?php echo anchor('Welcome/hapus/'.$row->id,'Delete'); ?> 
               </td>
             </tr>
@@ -115,10 +116,74 @@ defined('BASEPATH') OR exit('No direct script access allowed');
    </table>
  </div>
 
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content" style="width:42%">
+      <span class="close" style="color:black ;padding-left: 15px">&times;</span>
+    <div class="modal-body">
+      <div class="modal-header">
+      <h3>Tambah Siswa</h3> 
+      </div><br>
+      
+      <form method="post" action="<?php echo base_url(); ?>index.php/Welcome/tambah_aksi">
+        Nama :<br>
+        <input type="text" name="nama" style="border-radius : 5px 5px 5px 5px ; width: 530px"><br><br>
+        Kelas :<br>
+        <select id="cars" name="kelas" style="border-radius : 5px 5px 5px 5px ; width: 530px">
+          <option value="RPL" disable>RPL</option>
+          <option value="AP">AP</option>
+          <option value="TKJ" disabled>TKJ</option>
+        </select><br><br>
+        Nisn :<br>
+        <input type="text" name="nisn" style="border-radius : 5px 5px 5px 5px ; width: 530px"><br><br>
+        Nis :<br>
+        <input type="text" name="nis" style="border-radius : 5px 5px 5px 5px ; width: 530px"><br><br>
+        Alamat :<br>
+        <textarea style="border-radius : 5px 5px 5px 5px ; width: 530px; height: 100px;" name="alamat"></textarea><br><br>
+        No Telp :<br>
+        <input type="text" name="telp" style="border-radius : 5px 5px 5px 5px ; width: 530px"><br><br>
+        <div class="modal-footer">
+        <center><button type="submit" class="btn-primary" value="Tambah">Submit</button></center> 
+        </div>
+        
+      </form>
+
+    </div>
+  </div>
+
+</div>
 
 
 
+<script>
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
 
 <script>
 function openNav() {
